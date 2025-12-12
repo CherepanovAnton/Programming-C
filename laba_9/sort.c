add175a3e8645a67a4f3ad9fe25199c66f0f68c0
cerepanlvanton-cyber <cerepanlvanton@gmail.com>
2025-12-12 12:47:28 +0700

    Создал и добавил в область stage файл sort.c

---
+++ laba_9/sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Флаг для оптимизации - если за проход не было обменов, массив отсортирован
+        int swapped = 0;
+        
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Обмен элементов
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        
+        // Если не было обменов, выходим раньше
+        if (!swapped) break;
+    }
+}
