ee4ca9bbc5c48f46c891ab296b252b458e976ad3
cerepanlvanton-cyber <cerepanlvanton@gmail.com>
2025-12-12 12:53:18 +0700

    Добавил 2 комментария в sort.c

--- laba_9/sort.c
+++ laba_9/sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Сортировка методом Bubble sort
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         // Флаг для оптимизации - если за проход не было обменов, массив отсортирован
